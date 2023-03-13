54cb5b649ec43f9fd7254806ccf040308085b3d2
Megrathund <66427974+Megrathund@users.noreply.github.com>
2023-03-13 15:27:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from random import * # Import from the 'random'-module.
+from random import *
 
-validInput = False # Initilize and set few variabels.
+validInput = False
 gridWidth = "notSet"
 gridHeight = "notSet"
 wordTheme = "notSet"
@@ -9,7 +9,7 @@ userDefinedGeneration = "notSet"
 words = []
 
 
-while (validInput != True): # Reqeust: standard or userdefined?
+while (validInput != True):
     userDefinedGeneration = input("Moechten Sie Ihr Buchenstabensalat Benutzerdefiniert erstellen\noder mit den Standard-Einstellungen? [benutzerdefiniert/standard]\nIhre Antwort: ")
     if (userDefinedGeneration == "benutzerdefiniert"):
         userDefinedGeneration = "userDefined"
@@ -22,9 +22,9 @@ while (validInput != True): # Reqeust: standard or userdefined?
         userDefinedGeneration = "notSet"
 
 validInput = False
-if (userDefinedGeneration == "userDefined"): # Userdefined settings before generate:
+if (userDefinedGeneration == "userDefined"):
     while (validInput != True):
-        if (gridWidth == "notSet"): # The user set the Grid-width.
+        if (gridWidth == "notSet"):
             gridWidth = input("Bitte geben Sie die Breite des Grid-Systems an: ")
             if (int(gridWidth) < 15):
                 print("Die mindestbreite des Grid-System's ist unterschritten.\nBitte geben Sie eine hoehere Breite an.\n")
@@ -33,7 +33,7 @@ if (userDefinedGeneration == "userDefined"): # Userdefined settings before gener
                 print("Die maximalbreite des Grid-System's ist ueberschritten.\nBitte geben Sie eine geringere Breite an.\n")
                 gridWidth = "notSet"
 
-        elif (gridHeight == "notSet"): # The user set the Grid-height.
+        elif (gridHeight == "notSet"):
             gridHeight = input("Bitte geben Sie die Hoehe des Grid-Systems an: ")
             if (int(gridHeight) < 15):
                 print("Die mindesthoehe des Grid-System's ist unterschritten.\nBitte geben Sie eine hoehere Hoehe an.\n")
@@ -42,10 +42,10 @@ if (userDefinedGeneration == "userDefined"): # Userdefined settings before gener
                 print("Die maximalhoehe des Grid-System's ist ueberschritten.\nBitte geben Sie eine geringere Hoehe an.\n")
                 gridHeight = "notSet"
 
-        elif (wordTheme == "notSet"): # The user set the Wordtheme.
+        elif (wordTheme == "notSet"):
             wordTheme = input("Bitte geben Sie das zu verwendende Wort-Thema an: ")
 
-            if (wordTheme == "fruit"):  # Set the word-list.
+            if (wordTheme == "fruit"):
                 words = ["APFEL", "BIRNE", "BANANE", "KIRSCHE", "ORANGE", "KIWI", "PFLAUME", "MELONE", "PFIRSICH",
                          "TRAUBE",
                          "ANANAS", "ERDBEERE", "BROMBEERE", "HIMBEERE", "JOHANNISBEERE", "MIRABELLE", "NEKTARINE",
@@ -61,17 +61,17 @@ if (userDefinedGeneration == "userDefined"): # Userdefined settings before gener
                          "SCHMETTERLING", "MAUS", "HIRSCH", "WURM", "MAULWURF", "STINKTIER", "RAUPE", "MEISE",
                          "ZEBRA", "WACHSMOTTE", "JAGUAR", "ANTILOPE", "KAKADU", "SCHLANGE"]  # The list with the words for use with theme 'animal'.
 
-            elif (wordTheme == "jop"):
+            elif (wordTheme == "job"):
                 words = ["MALER", "SCHORNSTERINFEGER", "VERKAEUFER", "PROGRAMMIERER", "BAUARBEITER", "IMKER",
                          "HAUSMEISTER", "KAPITAEN", "FORSCHER", "JAEGER", "FOTOGRAF", "ARZT", "AUTOR", "BAHNFAHRER",
-                         "SOLDAT", "WISSENSCHAFTLER", "BAUER"]  # The list with the words for use with theme 'jop'.
+                         "SOLDAT", "WISSENSCHAFTLER", "BAUER"]
 
 
-            if (wordTheme != "fruit" and wordTheme != "in-the-air" and wordTheme != "animal" and wordTheme != "jop"): # Exists the choiced word-theme.
-                print("Dieses Wort-Thema gibt es nicht! Füge es hinzu\noder waehle aus den schon vorhandenen Wort-Themen:\n'fruit', 'in-the-air', 'animal', 'jop'\n")
+            if (wordTheme != "fruit" and wordTheme != "in-the-air" and wordTheme != "animal" and wordTheme != "job"):
+                print("Dieses Wort-Thema gibt es nicht! Füge es hinzu\noder waehle aus den schon vorhandenen Wort-Themen:\n'fruit', 'in-the-air', 'animal', 'job'\n")
                 wordTheme = "notSet"
 
-        elif (numberOfWords == "notSet"): # The user set the number of words there are used in the Buchstabensalat.
+        elif (numberOfWords == "notSet"):
             numberOfWords = input("Bitte geben Sie an wie viele Woerter verwendet werden sollen.\nSie koennen bis zu " + str(len(words)) + " Woerter aus dem Wort-Thema '" + wordTheme + "' " + "verwenden: ")
             if (int(numberOfWords) <= 0):
                 print("Ohne Woerter kann es keinen Buchenstabensalat geben!\n")
@@ -81,9 +81,9 @@ if (userDefinedGeneration == "userDefined"): # Userdefined settings before gener
                 numberOfWords = "notSet"
 
 
-        if (gridWidth != "notSet" and gridHeight != "notSet" and wordTheme != "notSet" and numberOfWords != "notSet"): # Checked up of the user-answers are correctly.
+        if (gridWidth != "notSet" and gridHeight != "notSet" and wordTheme != "notSet" and numberOfWords != "notSet"):
             validInput = True
-else: # Standard generation of the Buchstabensalat.
+else:
     gridWidth = 20
     gridHeight = 21
     wordTheme = "fruit"
@@ -91,53 +91,53 @@ else: # Standard generation of the Buchstabensalat.
              "TRAUBE",
              "ANANAS", "ERDBEERE", "BROMBEERE", "HIMBEERE", "JOHANNISBEERE", "MIRABELLE", "NEKTARINE",
              "MANDERINE",
-             "KAKI", "QUITTE"]  # The list with the words for use with theme 'fruit'.
+             "KAKI", "QUITTE"]
     numberOfWords = 20
 
 
-gridWidth = int(gridWidth) # Set the settings before generation of the Buchstabensalat.
+gridWidth = int(gridWidth)
 gridHeight = int(gridHeight)
 numberOfWords = int(numberOfWords)
-grid = [["0" for i in range(gridWidth)] for j in range(gridHeight)] # Generate of the Grid-System.
+grid = [["0" for i in range(gridWidth)] for j in range(gridHeight)]
 
-usedWords = [] # Here are the words were are used saved.
+usedWords = []
 
 
 
-def add_word(direction): # This function add the words in the Grid-System.
+def add_word(direction):
     freePlaces = "no"
     randomWord = "none"
     randomRow = 0
     randomField = 0
     trys_freeplaces = 0
 
-    while (freePlaces == "no"):  # In this loop, search for a free place in the Grid-System.
+    while (freePlaces == "no"):
         trys_freeplaces = trys_freeplaces + 1
         randomWord = words[randint(0, len(words)-1)]
         randomRow = randint(0, (len(grid)-1)-len(randomWord))
         randomField = randint(0, (len(grid[0])-1)-len(randomWord))
 
         trys_randomwords = 0
-        while (randomWord in usedWords): # Choose a random word were are not used.
+        while (randomWord in usedWords):
             randomWord = words[randint(0, len(words) - 1)]
 
-            if (trys_randomwords >= len(words)*10): # It's checked up of happen a problem with the choose a random word. If it gives a problem, then stopped the program and print a error.
+            if (trys_randomwords >= len(words)*10):
                 exit("FEHLER! Es gibt keine Wörter mehr die in das Kreuzworträtsel hinzugefügt werden können\n"
                      "ohne das es doppelte Wörter gibt. Reduziere entweder die Anzahl der zu generierenden Wörter\n"
                      "oder erweitere die Liste der Wörter die benutzt werden können (Listenname 'words'). (Nur im Quellcode möglich)")
             trys_randomwords = trys_randomwords + 1
 
-        if (trys_freeplaces >= len(grid)*len(grid[0])+1000): # It's checked up of happen a problem. There, it's give an error and stop the programm.
+        if (trys_freeplaces >= len(grid)*len(grid[0])+1000):
             exit("FEHLER! Die Wörter konnten nicht zusammengestellt werden.\n"
                  "Möglicherweise sind es zu viele Wörter.\n"
                  "Bitte reduzieren Sie die Anzahl der Wörter.")
 
-        counterHorizontal = 0 # Counter reset.
-        counterVertical = 0 # Counter reset.
+        counterHorizontal = 0
+        counterVertical = 0
 
-        for i in range(0, len(randomWord)): # It's search up free places for a word in the Grid-System.
+        for i in range(0, len(randomWord))
             testChar = list(randomWord)[i]
-            try: # If it given a index-error then generate a new random position and a new random word.
+            try:
                 if (grid[randomRow+counterVertical][randomField+counterHorizontal] == "0"):
                     freePlaces = "yes"
                 elif (grid[randomRow+counterVertical][randomField+counterHorizontal] == testChar):
@@ -149,17 +149,17 @@ def add_word(direction): # This function add the words in the Grid-System.
                 freePlaces = "no"
                 break
 
-            if (direction == "horizontal"): # Change the counter.
+            if (direction == "horizontal"):
                 counterHorizontal = counterHorizontal + 1
             else:
                 counterVertical = counterVertical + 1
 
 
-    counterHorizontal = 0 # Again a counter-reset and add the used word to the 'usedWords'-list.
+    counterHorizontal = 0
     counterVertical = 0
     usedWords.append(randomWord)
 
-    for char in randomWord: # Add the word to the Grid.
+    for char in randomWord:
         grid[randomRow+counterVertical][randomField+counterHorizontal] = char
         if (direction == "horizontal"):
             counterHorizontal = counterHorizontal + 1
@@ -169,7 +169,7 @@ def add_word(direction): # This function add the words in the Grid-System.
 
 
 
-def generate_words(words): # This function make the adding of words easyer, there just start this function and given the parameter 'words' with the number of words.
+def generate_words(words):
     lastDirection = "vertical"
 
     for i in range(0, words):
@@ -182,7 +182,7 @@ def generate_words(words): # This function make the adding of words easyer, ther
 
 
 
-def generate_chars(): # These function replaces all '0'-elements in the grid-system with a random char.
+def generate_chars():
     chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     gridRow = 0
     gridField = 0
@@ -199,7 +199,7 @@ def generate_chars(): # These function replaces all '0'-elements in the grid-sys
 
 
 
-def save_result(): # This function save the 'Buchenstabensalat'-Puzzle in the 'buchstabensalat.txt'-file.
+def save_result():
     i = 0
     with open("./buchstabensalat.txt", "w", encoding="utf-8") as file:
         file.write("Die folgenden Wörter müssen gefunden werden:\n\n")
@@ -217,18 +217,18 @@ def save_result(): # This function save the 'Buchenstabensalat'-Puzzle in the 'b
                 file.write(field.strip())
             file.write("\n")
 
-generate_words(words=numberOfWords) # This commands call all need functions to generate a full-ready 'Buchenstabensalat'.
+generate_words(words=numberOfWords)
 generate_chars()
 save_result()
 
 
-print("\t\t\t\t\t\tDie folgenden Wörter müssen gefunden werden:") # Printing texts.
+print("\t\t\t\t\t\tDie folgenden Wörter müssen gefunden werden:")
 print(usedWords)
 print("\n")
 
-for line in grid: # Grid-system printing on the Terminal.
+for line in grid:
     print(line)
 
-print("Ihr Buchstabensalat wurde erfolgreich generiert!\n" # Last, few prints on the Terminal :)!
+print("Ihr Buchstabensalat wurde erfolgreich generiert!\n"
       "Öffnen Sie nun die 'buchstabensalat.txt'-Datei und kopieren Sie das Rätsel in z.B. eine *.docx-Datei\n"
       "welche Sie nun ausdrucken können. Viel Spaß beim rätseln :)!")
